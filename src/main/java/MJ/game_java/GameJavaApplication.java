016c5c1ee01061ea390c81d1b73d3f4d42a49fda
mjBae <bmj5208@gmail.com>
2021-02-04 22:19:13 +0900

    Add: 프로젝트 테스트

--- src/main/java/MJ/game_java/GameJavaApplication.java
+++ src/main/java/MJ/game_java/GameJavaApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GameJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GameJavaApplication.class, args);
+
+		System.out.println("test");
 	}
 
 }
