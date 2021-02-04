c0f827157f087908c9847cf2d8bb9d0c01e374db
mjBae <bmj5208@gmail.com>
2021-02-04 22:05:46 +0900

    Init Project

---
+++ src/main/java/MJ/game_java/GameJavaApplication.java
@@ -0,0 +1,13 @@
+package MJ.game_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MJ/game_java/GameJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package MJ.game_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
