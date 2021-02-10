485b7c142efbe6b3fd003d44ba93c8a65aab81b7
MJbae <gently.gogo@gmail.com>
2021-02-10 14:36:57 +0900

    Test: test case 생성

---
+++ src/main/java/MJ/chess/Color.java
@@ -0,0 +1,2 @@
+package MJ.chess;public enum Color {
+}
---
+++ src/main/java/MJ/chess/Pawn.java
@@ -0,0 +1,13 @@
+package MJ.game_java;
+
+public class Pawn {
+    String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
